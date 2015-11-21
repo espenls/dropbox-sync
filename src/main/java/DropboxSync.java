11706ac23c691609807017e59d0af9b7dd9ba5ea
espsegel <espen.larsen.segelvik@finn.no>
2015-11-22 00:46:04 +0100

    Deleting files from disk when deleted from dropbox

--- src/main/java/DropboxSync.java
+++ src/main/java/DropboxSync.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import com.dropbox.core.DbxClient;
@@ -12,7 +14,7 @@ import com.dropbox.core.DbxRequestConfig;
 
 public class DropboxSync {
 
-    // Get your app key and secret from the Dropbox developers website.
+    // Get your ACCESS_TOKEN from the Dropbox developers website.
     public static final String ACCESS_TOKEN = "ACCESS_TOKEN";
     public static final String SYNC_FOLDER = "sync";
 
@@ -31,15 +33,35 @@ public class DropboxSync {
 
         createSyncFolder();
 
+        List<String> filesOnDropbox = new ArrayList<String>();
         for (DbxEntry child : listing.children) {
             System.out.println("	" + child.name + ": " + child.toString());
 
             File f = new File("sync/" +child.asFile().name);
             if(!f.exists()) {
-                client.getFile(child.asFile().path, child.asFile().rev, new FileOutputStream("sync/" +child.name));
+                client.getFile(child.asFile().path, child.asFile().rev, new FileOutputStream(SYNC_FOLDER + "/" + child.name));
+            }
+            filesOnDropbox.add(SYNC_FOLDER + "/" + child.name);
+        }
+
+        deleteFilesFromDisk(getStoredFilesOnDisk(), filesOnDropbox);
+    }
+
+    private static File[] getStoredFilesOnDisk(){
+        File syncFolder = new File(SYNC_FOLDER);
+        return syncFolder.listFiles();
+    }
+
+    public static void deleteFilesFromDisk(File[] filesOnDisk, List<String> filesOnDropBox){
+
+        for(File file: filesOnDisk){
+            if(!filesOnDropBox.contains(SYNC_FOLDER + "/" +file.getName())){
+                System.out.println("Deleting file: " + file.getName());
+                file.delete();
             }
         }
     }
+
 
     private static void createSyncFolder() {
         File syncFolder = new File(SYNC_FOLDER);
